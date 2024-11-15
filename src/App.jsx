4de44a52910499323a3166633d5ff6d846e248d9
Suyog Basnet <slayer@Suyogs-MacBook-Air.local>
2024-11-15 18:32:16 +0545

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,33 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import SearchResults from './components/SearchResults';
 import { busRoutes } from './data/busRoutes';
 import BusRouteMap from './components/BusRouteMap';
-import { MainContextProvider } from "./context/primaryContext";
+import { MainContextProvider } from './context/primaryContext';
 import ContributePage from './components/ContributePage';
 
-// Move the state management to the main App component
 const App = () => {
   const [searchParams, setSearchParams] = useState({ from: '', to: '' });
   const [showResults, setShowResults] = useState(false);
 
   const handleSearch = (params) => {
-    console.log("Search parameters:", params);
+    console.log('Search parameters:', params);
     setSearchParams(params);
     setShowResults(true);
   };
 
   const filteredRoutes = busRoutes.filter(
-    route => 
+    (route) =>
       route.from.toLowerCase().includes(searchParams.from.toLowerCase()) &&
       route.to.toLowerCase().includes(searchParams.to.toLowerCase())
   );
 
-  return (
-    <>
-    <div className="relative min-h-screen">
-      <div className="flex bg-gray-100 pb-24"> {/* Added pb-24 for bottom spacing */}
-        <Sidebar
-          onSearch={handleSearch}
-          searchParams={searchParams}
-          setSearchParams={setSearchParams}
-        />
-              <MainContextProvider>
-        <BusRouteMap />
-      </MainContextProvider>
-
-        <div className="flex-1 p-8">
-          {showResults && (
-            <SearchResults 
-              routes={filteredRoutes} 
-              searchParams={searchParams}
-              
-            />
-          )}
-  // MainLayout component with props
   const MainLayout = () => {
     const location = useLocation();
-    
-    // If returning from contribute page, maintain the showResults state
+
+    // Maintain `showResults` state when returning from the contribute page
     React.useEffect(() => {
       if (location.state?.fromContribute) {
         setShowResults(true);
@@ -65,21 +42,17 @@ const App = () => {
             searchParams={searchParams}
             setSearchParams={setSearchParams}
           />
-
+          <MainContextProvider>
+            <BusRouteMap />
+          </MainContextProvider>
           <div className="flex-1 p-8">
             {showResults && (
-              <SearchResults 
-                routes={filteredRoutes} 
-                searchParams={searchParams}
-              />
+              <SearchResults routes={filteredRoutes} searchParams={searchParams} />
             )}
           </div>
         </div>
         {showResults && <BottomFeatures />}
       </div>
-      {showResults && <BottomFeatures />}
-    </div>
-    </>
     );
   };
 
@@ -87,14 +60,14 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />} />
-        <Route 
-          path="/contribute" 
+        <Route
+          path="/contribute"
           element={
-            <ContributePage 
+            <ContributePage
               searchParams={searchParams}
               showResults={showResults}
             />
-          } 
+          }
         />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
