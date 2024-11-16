3d662b017455beba26384a688150ae563451f5b7
sthaarekh <arekhshrestha@gmail.com>
2024-11-16 13:02:13 +0545

    Random commit

--- src/components/BusInfoPanel.jsx
+++ src/components/BusInfoPanel.jsx
@@ -6,13 +6,6 @@ const BusInfoPanel = ({ selectedRoute }) => {
 
   if (!selectedRoute) return null;
 
-  // Add this handler function
-  const handleContributeClick = () => {
-    navigate('/contribute', { 
-      state: { selectedRoute } // Pass the selectedRoute as state
-    });
-  };
-
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg border-t border-gray-200 p-6">
       <div className="max-w-7xl mx-auto">
@@ -48,16 +41,6 @@ const BusInfoPanel = ({ selectedRoute }) => {
                 <span>{`NPR ${parseInt(selectedRoute.price.split(' ')[1]) + 50}`}</span>
               </div>
             </div>
-          </div>
-
-          {/* Contribute Button - Updated with onClick handler */}
-          <div className="flex-1 flex justify-end">
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-lg font-medium transition duration-200"
-              onClick={handleContributeClick} // Add the click handler here
-            >
-              Contribute Information
-            </button>
           </div>
         </div>
       </div>
